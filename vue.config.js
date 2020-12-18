24cfa941432a5ada0b482fe6b32d12e06996bdcb
isalavatov <afemi-oke@playnext.ru>
2020-12-18 21:35:31 +0300

    upd: optimization to single file

--- vue.config.js
+++ vue.config.js
@@ -10,3 +10,14 @@
 //   },
 //   "filenameHashing": false,
 // }
+
+module.exports = {
+  configureWebpack: {
+    optimization: {
+      splitChunks: false
+    }
+  },
+  css: {
+    extract: false,
+  },
+}
